cdd6eb216e224d372194328e3575f20c38956cfd
Tom <tompowellgames@gmail.com>
2020-01-27 22:07:23 +1300

    Context now fetches photos

--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,19 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 const { Provider, Consumer } = React.createContext();
 
-class ContextProvider extends Component {
-  state = {
-    photos: []
-  };
-  render() {
-    return <Provider value={this.state.photos}>{this.props.children}</Provider>;
-  }
+function ContextProvider({ children }) {
+  const [allPhotos, setAllPhotos] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    )
+      .then(response => response.json())
+      .then(data => setAllPhotos(data));
+  }, []);
+
+  console.log(allPhotos);
+
+  return <Provider value={{ allPhotos }}>{children}</Provider>;
 }
 
 export { ContextProvider, Provider, Consumer };
