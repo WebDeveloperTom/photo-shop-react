77eafb48a884d9bb6842e602a0b6b33db16d4f4f
Tom <tompowellgames@gmail.com>
2020-02-01 21:34:38 +1300

    create conditional cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../Context";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const context = useContext(Context);
   return (
     <header>
       <h2>
         <Link to="/">Pic Some</Link>
       </h2>
       <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        {context.cart.length > 0 ? (
+          <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
+        ) : (
+          <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        )}
       </Link>
     </header>
   );
